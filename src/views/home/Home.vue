05dce767a90faadbf80492a5543fd0c4a95986a8
pgl <wb-gongyuli@cai-inc.com>
2021-03-06 17:45:34 +0800

    初始化项目基本配置

---
+++ src/views/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <Header />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Header from './components/header.vue';
+
+@Component({
+  components: {
+    Header,
+  },
+})
+export default class Home extends Vue {}
+</script>
---
+++ src/views/home/components/header.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="header">
+      xxx
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Header extends Vue {}
+</script>
